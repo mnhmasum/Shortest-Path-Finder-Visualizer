aeac0d4cf6bcd2d2491007cb812823cc875ae2f6
Nazmul <nhasan@ael-bd.com>
2020-10-22 19:17:46 +0600

    Change app title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Dijkstra’s Simulator - Flutter'),
+      home: MyHomePage(title: 'Dijkstra Short Path Visualizer - Flutter'),
     );
   }
 }
